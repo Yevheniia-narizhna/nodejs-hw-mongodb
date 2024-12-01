03e500f63dc8ad57fba169a1f13d18d67502c3d1
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2024-12-01 12:03:40 +0200

    thelast

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import express from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 // import { getAllContacts, getContactById } from './services/contacts.js';
 import {
@@ -9,16 +9,21 @@ import {
   patchContactController,
 } from '../controllers/contacts.js';
 
-const router = Router();
+const router = express.Router();
+const jsonParser = express.json();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 
 router.get('/contacts/:contactId', ctrlWrapper(getContactsByIdController));
 
-router.post('/contacts', ctrlWrapper(createContactController));
+router.post('/contacts', jsonParser, ctrlWrapper(createContactController));
 
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.patch(
+  '/contacts/:contactId',
+  jsonParser,
+  ctrlWrapper(patchContactController),
+);
 
 export default router;
