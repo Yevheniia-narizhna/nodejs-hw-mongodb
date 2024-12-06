28d8aa3dc8d8ee43bf53781e2e9fa5e64cc08e41
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2024-12-06 21:20:54 +0200

    filter-sort

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,15 +1,30 @@
 import { Contact } from '../models/contact.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
+import { SORT_ORDER } from '../constants/index.js';
 
-export const getAllContacts = async ({ page = 1, perPage = 10 }) => {
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+  filter = {},
+}) => {
   const limit = perPage;
   const skip = page > 0 ? (page - 1) * perPage : 0;
 
   const contactsQuery = Contact.find();
 
+  if (filter.isFavourite !== undefined) {
+    contactsQuery.where('isFavourite').equals(filter.isFavourite);
+  }
+
   const [contactsCount, contacts] = await Promise.all([
     Contact.find().merge(contactsQuery).countDocuments(),
-    contactsQuery.skip(skip).limit(limit),
+    contactsQuery
+      .skip(skip)
+      .limit(limit)
+      .sort({ [sortBy]: sortOrder })
+      .exec(),
   ]);
 
   const paginationData = calculatePaginationData(contactsCount, perPage, page);
