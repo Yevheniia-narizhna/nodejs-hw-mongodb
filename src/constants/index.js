99b2149a08c54a0c7f6f94682f59c41299f90fcd
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2024-12-17 14:18:15 +0200

    minutes

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@ export const SORT_ORDER = {
   DESC: 'desc',
 };
 
-export const FIFTEEN_MINUTES = 15 * 60 * 1000;
+export const FIFTEEN_MINUTES = 180 * 60 * 1000;
 export const ONE_DAY = 24 * 60 * 60 * 1000;
 export const THIRTY_DAYS = 30 * ONE_DAY;
 
